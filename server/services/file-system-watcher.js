bb40c8bd567e367f06b527c38b05eaedcf74923d
Bowen Wang <iwangbowen@gmail.com>
2018-11-12 14:20:36 +0800

    Hide specified files in file watcher

--- server/services/file-system-watcher.js
+++ server/services/file-system-watcher.js
@@ -13,18 +13,21 @@ var watcher = chokidar.watch(root, {
     // Once with a single argument (the path),
     // second time with two arguments (the path and the fs.Stats object of that path).
     if (stat) {
-      var isDir = stat.isDirectory()
-      var fso = new FileSystemObject(path, stat)
-
+      var isDir = stat.isDirectory();
+      var fso = new FileSystemObject(path, stat);
       if (isDir) {
         // Otherwise ignore node_modules and bower_components and also ignore
         // any dir starting with a '.' e.g. '.git'
         return (fso.name === 'node_modules' && root === fso.dir) ||
-          fso.name === 'bower_components' || /[\/\\]\./.test(path)
+          fso.name === 'bower_components' || /[\/\\]\./.test(path);
       } else {
         return fso.name === '.DS_Store'
           || fso.name === 'noide.exe'
-          || fso.name.includes('noide.config');
+          || fso.name === 'noide.zip'
+          || fso.name.includes('noide.config')
+          || fso.name === 'startup.bat'
+          || fso.name === 'shutdown.bat'
+          || fso.name === 'RunHide.vbs';
       }
     }
     return false;
